199029539b370491358dd2b1d86d057039273329
Свириденко Максим <max.s-vir@yandex.ru>
2021-11-29 23:21:10 +0300

    Create omp_pi_atomic_4.c

---
+++ omp_pi_atomic_4.c
@@ -0,0 +1,27 @@
+#include <stdio.h>
+#include <omp.h>
+#include <math.h>
+#define n 100000000
+int main(int argc, char *argv[])
+{
+    static int i;
+    static double x, ans, sum, thread_sum, step;
+
+    step = 1.0/(double)n;
+    sum = 0.0;
+    thread_sum = 0.0;
+
+#pragma omp parallel private(x) firstprivate(thread_sum)
+    {
+#pragma omp for
+    for (i = 1; i <= n; i++) {
+	x = (i - 0.5) * step;
+	thread_sum += exp(x);
+      }                                                    
+#pragma omp atomic
+    sum += thread_sum;
+    }
+    ans = step * sum;
+//    printf("Value of the integral: %f\n", pi);
+    return 0;
+}
